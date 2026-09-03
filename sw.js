2f4e96bb53a2d8689fc6d2d4914b6fe1ebf71c43
Vivek Mishra <regionalcflmis@gmail.com>
2026-09-03 22:50:53 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='moneywise-pwa-shell-v4.7.0-r1';';
-const CORE=['./','./index.html','./404.html','./config.js?v=4701','./manifest.webmanifest?v=4701','./offline.html','./icon-192.png?v=4701','./icon-512.png?v=4701','./apple-touch-icon.png?v=4701'];
+const CACHE='moneywise-pwa-shell-v4.7.0-r2';
+const CORE=['./','./index.html','./404.html','./config.js?v=4702','./manifest.webmanifest?v=4702','./offline.html','./icon-192.png?v=4702','./icon-512.png?v=4702','./apple-touch-icon.png?v=4702'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
