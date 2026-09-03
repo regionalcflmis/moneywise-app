762781a740036d827feb4ad70b736135c7c33621
Vivek Mishra <regionalcflmis@gmail.com>
2026-09-03 21:36:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.MONEYWISE_PWA_CONFIG = Object.freeze({
-  appUrl: 'https://script.google.com/macros/s/AKfycbxwRw75lksQhHN2tWngA-W9bOFZlzejLzr77LMPCehqDJkUd9b_c6lGBXFf4GgVdHud/exec',
+  appUrl: 'https://script.google.com/macros/s/AKfycbyWeFRIbiQx5iWkCRR6BJiRtzA4Wp4HNTVVaQzntacWv9YU1DHnaqxvlGns6LPtZk4K/exec',
   shellVersion: '4.5.4-pwa',
   appName: 'MoneyWise CFL Monitoring'
 });
