92204318904614d74701dab4a934b1a0da91c313
Vivek Mishra <regionalcflmis@gmail.com>
2026-09-03 22:28:42 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='moneywise-pwa-shell-v4.7.0-r1';
+const CACHE='moneywise-pwa-shell-v4.7.0-r1';';
 const CORE=['./','./index.html','./404.html','./config.js?v=4701','./manifest.webmanifest?v=4701','./offline.html','./icon-192.png?v=4701','./icon-512.png?v=4701','./apple-touch-icon.png?v=4701'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
