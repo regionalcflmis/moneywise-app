c67f69047ba3274e0c58c7187dadf063e5a7b8ca
Vivek Mishra <regionalcflmis@gmail.com>
2026-09-09 22:20:51 +0530

    Add files via upload

---
+++ README_PWA_V5_7_2_HINDI.txt
@@ -0,0 +1,7 @@
+MoneyWise PWA v5.7.2
+
+GitHub repository ROOT me ZIP ki sabhi files replace karein.
+Test URL:
+https://regionalcflmis.github.io/moneywise-app/?v=5721
+
+AM/CBO login me Overview & Reports option ab visible hai.
--- sw.js
+++ sw.js
@@ -1,12 +1,6 @@
-// MoneyWise v5.5.3
-// This service worker intentionally does not cache the launcher.
-// It also removes previous MoneyWise shell caches on activation.
+// MoneyWise PWA v5.7.2
 self.addEventListener('install',event=>self.skipWaiting());
 self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
 self.addEventListener('fetch',()=>{});
