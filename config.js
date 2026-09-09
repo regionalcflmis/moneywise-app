c221e0f7012aeacb4af8cd7263a5b513101aad1e
Vivek Mishra <regionalcflmis@gmail.com>
2026-09-09 09:40:08 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.MONEYWISE_PWA_CONFIG = Object.freeze({
-  appUrl: 'https://script.google.com/macros/s/AKfycbyWeFRIbiQx5iWkCRR6BJiRtzA4Wp4HNTVVaQzntacWv9YU1DHnaqxvlGns6LPtZk4K/exec',
-  shellVersion: '5.5.2-pwa',
+  appUrl: "https://script.google.com/macros/s/AKfycbyWeFRIbiQx5iWkCRR6BJiRtzA4Wp4HNTVVaQzntacWv9YU1DHnaqxvlGns6LPtZk4K/exec",
+  shellVersion: '5.5.3-pwa',
   appName: 'MoneyWise CFL Monitoring'
 });
